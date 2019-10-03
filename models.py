81f0bcced2535800ccb5e4c2bfa112703930b122
laurelwoods <laurelwoods0102@naver.com>
2019-10-03 21:01:19 +0900

    first model

--- models.py
+++ models.py
@@ -7,9 +7,10 @@ import tensorflow as tf
 from tensorflow import keras
 
 df = pd.read_csv("./dataset/dataset_hide-on-bush_myPick_ex.csv")
+df = df.astype(float)
 
-train, test = train_test_split(df, test_size=0.1)
-train, val = train_test_split(train, test_size=0.1)
+train_df, test_df = train_test_split(df, test_size=0.1)
+train_df, val_df = train_test_split(train_df, test_size=0.1)
 
 def df_to_dataset(dataframe, shuffle=True, batch_size=32):
     #dataframe = dataframe.copy()
@@ -22,7 +23,46 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=32):
 
     return dataset
 
-train_ds = df_to_dataset(train, shuffle=True)
-val_ds = df_to_dataset(val, shuffle=False)
-test_ds = df_to_dataset(test, shuffle=False)
+train_ds = df_to_dataset(train_df, shuffle=True)
+val_ds = df_to_dataset(val_df, shuffle=False)
+test_ds = df_to_dataset(test_df, shuffle=False)
 
+def pack_features_vector(features, labels):
+    features = tf.stack(list(features.values()), axis=1)
+    return features, labels
+
+train_dataset = train_ds.map(pack_features_vector)
+#for features, labels in train_dataset.take(1):
+#    print(features)
+
+
+class LogisticLayer(keras.layers.Layer):
+    def __init__(self, input_dim=146, num_outputs=1):
+        super(LogisticLayer, self).__init__()
+        w_init = tf.initializers.GlorotUniform()     # NOTICE : weight matrix itself contains bias
+        self.w = tf.Variable(
+            initial_value=w_init(shape=(input_dim, num_outputs), dtype='float32'),
+            trainable=True)
+        #print(self.w)
+
+    def call(self, input_features):
+        return tf.matmul(input_features, self.w)
+'''
+layer = LogisticLayer()
+for features, lables in train_dataset.take(1):
+    print("forward", layer(features))
+    print("weights", layer.trainable_variables)
+'''
+
+class LogisticModel(tf.keras.Model):
+    def __init__(self):
+        super(LogisticModel, self).__init__()
+        self.w = LogisticLayer()
+    
+    def call(self, input_features, training=False):
+        x = self.w(input_features)
+        return x
+
+model = LogisticModel()
+for features, labels in train_dataset.take(1):
+    print(model.trainable_variables)
