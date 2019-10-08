36b0a492d22d105cc89358a8a3eb067f4304a60d
laurelwoods <laurelwoods0102@naver.com>
2019-10-08 15:28:35 +0900

    accuracy

--- models.py
+++ models.py
@@ -2,15 +2,10 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 from tensorflow import keras
-
-df = pd.read_csv("./dataset/dataset_hide-on-bush_enemy_ex.csv")
-df = df.astype(float)
-
-train_df, test_df = train_test_split(df, test_size=0.1)
-train_df, val_df = train_test_split(train_df, test_size=0.1)
 
 def df_to_dataset(dataframe, shuffle=True, batch_size=32):
     #dataframe = dataframe.copy()
@@ -23,18 +18,9 @@ def df_to_dataset(dataframe, shuffle=True, batch_size=32):
 
     return dataset
 
-train_ds = df_to_dataset(train_df, shuffle=True)
-val_ds = df_to_dataset(val_df, shuffle=False)
-test_ds = df_to_dataset(test_df, shuffle=False)
-
 def pack_features_vector(features, labels):
     features = tf.stack(list(features.values()), axis=1)
     return features, labels
-
-train_dataset = train_ds.map(pack_features_vector)
-#for features, labels in train_dataset.take(1):
-#    print(features)
-
 
 class LogisticLayer(keras.layers.Layer):
     def __init__(self, input_dim=146, num_outputs=1):
@@ -45,61 +31,111 @@ class LogisticLayer(keras.layers.Layer):
             trainable=True)
         #print(self.w)
 
-    def call(self, input_features):
-        return tf.matmul(input_features, self.w)
-'''
-layer = LogisticLayer()
-for features, lables in train_dataset.take(1):
-    print("forward", layer(features))
-    print("weights", layer.trainable_variables)
-'''
+    def call(self, features):
+        return tf.matmul(features, self.w)
 
 class LogisticModel(tf.keras.Model):
     def __init__(self):
         super(LogisticModel, self).__init__()
         self.logistic_layer = LogisticLayer()
     
-    def call(self, input_features, training=False):
-        x = self.logistic_layer(input_features)
+    def call(self, features, training=False):
+        x = self.logistic_layer(features)
         return x
 
 def hypothesis(logit):
     return tf.divide(1.0, 1.0 + tf.exp(-1.0*logit))
 
-def cost(logit_tensor, input_labels):
-    hypo = hypothesis(logit_tensor)
-    #return -tf.reduce_mean(input_labels*tf.math.log(hypo) + (1.0-input_labels)*tf.math.log(1.0-hypo))
-    return -tf.reduce_mean(tf.math.log(hypo)*input_labels + tf.math.log(1.0-hypo)*(1.0-input_labels))
+def cost(logits, labels):
+    hypo = hypothesis(logits)
+    #return -tf.reduce_mean(labels*tf.math.log(hypo) + (1.0-labels)*tf.math.log(1.0-hypo))
+    return -tf.reduce_mean(tf.math.log(hypo)*labels + tf.math.log(1.0-hypo)*(1.0-labels))
 
-def grad(model, features, labels):
+#def grad(model, features, labels):
+def grad(logits, labels):
     with tf.GradientTape() as tape:
         cost_value = cost(model(features), labels)
     return cost_value, tape.gradient(cost_value, model.trainable_variables)
 
-def train(model, features, labels, learning_rate=0.01):
+def train(model, features, labels):
     with tf.GradientTape() as t:
         current_cost = cost(model(features), labels)
     dW = t.gradient(current_cost, [model.logistic_layer.w])
+    #print("current W : ", model.logistic_layer.w)
     optimizer.apply_gradients(zip(dW, [model.logistic_layer.w]))
-    print("current cost :", current_cost)
+    #print("trained W : ", model.logistic_layer.w)    
+    return current_cost
 
-model = LogisticModel()
-optimizer = tf.optimizers.Adam(learning_rate=0.01)
-for features, labels in train_dataset.take(1):
-    #logit = model(features)
-    labels = tf.dtypes.cast(labels, tf.float32)
-    labels = tf.reshape(labels, [32, 1])
-    labels = tf.transpose(labels)
-    #cost_value = cost(logit, labels)
+def batch_accuracy(hypos, labels, batch_size=32):
+    accuracy = 0
+    predictor = zip(hypos, labels)
+    for hypo, label in predictor:
+        prediction = 0
+        if hypo >= 0/5: prediction = 1
 
-    #print("Logit : ", logit)
+        if prediction == label: 
+            accuracy += 1
+        #print(hypo, " => ", prediction, " : ", label)
+    return accuracy/batch_size
 
-    #print("initial_cost :", cost_value)
 
-    #cost_value, grads = grad(model, features, labels)
-    #optimizer.minimize(cost(model(features), labels), model.logistic_layer.w)
-    train(model, features, labels)
-    re_logit = model(features, labels)
-    re_cost = cost(re_logit, labels)
-    print("re_cost : ", re_cost)
-    #print(model.logistic_layer.w)
+
+if __name__ == "__main__":
+    df = pd.read_csv("./dataset/dataset_hide-on-bush_enemy_ex.csv")
+    df = df.astype(float)
+
+    train_df, test_df = train_test_split(df, test_size=0.1)
+    train_df, val_df = train_test_split(train_df, test_size=0.1)
+
+
+    train_ds = df_to_dataset(train_df, shuffle=True)
+    val_ds = df_to_dataset(val_df, shuffle=False)
+    test_ds = df_to_dataset(test_df, shuffle=False)
+
+
+    train_dataset = train_ds.map(pack_features_vector)
+    #for features, labels in train_dataset.take(1):
+    #    print(features)
+
+    ex_ds = df_to_dataset(df, shuffle=True)
+    ex_dataset = ex_ds.map(pack_features_vector)
+
+
+    model = LogisticModel()
+    optimizer = tf.optimizers.Adam(learning_rate=0.01)
+
+    f = open('./model_results/version_1/cost.csv', 'w', newline='')
+    w = open('./model_results/version_1/weights.csv', 'w', newline='')
+
+    Ws = list()
+    train_cost_results = list()
+    train_accuracy_results = list()
+
+    epochs = range(10)
+    for epoch in epochs:
+        print('epoch : ', epoch)
+        temp_accuracy = list()
+        temp_cost = list()
+        for features, labels in ex_dataset:
+            labels = tf.dtypes.cast(labels, tf.float32) # reshape Labels
+            labels = tf.reshape(labels, [32, 1])
+            labels = tf.transpose(labels)
+
+            #current_cost = cost(model(features), labels)
+            current_cost = train(model, features, labels)
+
+            logits = model(features)
+            hypos = hypothesis(logits)
+
+            accuracy = batch_accuracy(tf.reshape(hypos, [1, 32]).numpy()[0], labels.numpy()[0])
+            print("accuracy : ", accuracy)
+
+            temp_accuracy.append(accuracy)
+            temp_cost.append(current_cost.numpy())
+        
+        train_accuracy_results.extend(temp_accuracy)
+        train_cost_results.extend(temp_cost)
+
+    plt.plot(train_accuracy_results)
+    plt.plot(train_cost_results)
+    plt.show()
