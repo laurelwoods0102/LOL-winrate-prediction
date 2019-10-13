b8f00f2b563ae1f0eab7c2c7e44eb1dbdf6fe8f6
laurelwoods <laurelwoods0102@naver.com>
2019-10-14 04:39:20 +0900

    KFold Cross Validation

--- fold.py
+++ fold.py
@@ -5,20 +5,23 @@ from sklearn.model_selection import KFold
 import tensorflow as tf
 from tensorflow import keras
 
-
-def df_to_dataset(dataframe, batch_size=32):
+'''
+def df_to_dataset(dataframe, batch=True):
     #dataframe = dataframe.copy()
     response = dataframe.pop('y')    
     dataset = tf.data.Dataset.from_tensor_slices((dict(dataframe), response.values))
 
     #if shuffle:
         #dataset = dataset.shuffle(buffer_size=len(dataframe))
-    dataset = dataset.batch(batch_size)
+    if batch:
+        batch_size = 32
+        dataset = dataset.batch(batch_size)
 
     return dataset
 
 def pack_features_vector(features, labels):
     features = tf.stack(list(features.values()), axis=1)
+
     return features, labels
 
 
@@ -36,36 +39,22 @@ if test_size == 0:
 train_df = df.loc[:train_len*batch_size-1]
 test_df = df.loc[train_len*batch_size-1:]
 
-train_ds = df_to_dataset(train_df)
-test_ds = df_to_dataset(test_df)
-
-train_dataset = train_ds.map(pack_features_vector)
-test_dataset = test_ds.map(pack_features_vector)
-
-for i in range(train_len):
-    for features, labels in train_dataset.skip(i):
-        print("Training")
-        print(features)
-        print(labels)
-        break
-    for features, labels in train_dataset.take(i):
-        print("Validation")
-        print(features)
-        print(labels)
-        break
-
-for features, labels in test_dataset:
-    print("Test")
-    print(features)
-    print(labels)
+kf = KFold(n_splits=train_len)
+for train_index, val_index in kf.split(train_df):
+    #print(train_df.iloc[test_index])
+    val_ds = df_to_dataset(train_df.iloc[val_index])
+    val_dataset = val_ds.map(pack_features_vector)
+    for f, l in val_dataset:
+        print(l)
     break
-
 '''
-train_ds = df_to_dataset(train_df, shuffle=True)
-train_dataset = train_ds.map(pack_features_vector)
 
-val = train_dataset.skip(1)
-for f, l in val:
-    print(f)
-    print(l)
-'''
+df = pd.read_csv("./dataset/test.csv")
+df = df.astype(float)
+df = df.sample(frac=1).reset_index(drop=True)
+
+response = df.pop('y')
+print(response.values)
+
+for d in df.values:
+    print(tf.convert_to_tensor(d))
--- models.py
+++ models.py
@@ -1,26 +1,42 @@
 import csv
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 import tensorflow as tf
 from tensorflow import keras
 
-def df_to_dataset(dataframe, shuffle=True, batch_size=32):
-    #dataframe = dataframe.copy()
-    response = dataframe.pop('result')    
+def df_to_dataset(dataframe, batch_size=32):
+    dataframe = dataframe.copy()
+    response = dataframe.pop('y')    
     dataset = tf.data.Dataset.from_tensor_slices((dict(dataframe), response.values))
 
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=len(dataframe))
+    #if shuffle:
+        #dataset = dataset.shuffle(buffer_size=len(dataframe))
     dataset = dataset.batch(batch_size)
 
     return dataset
 
 def pack_features_vector(features, labels):
     features = tf.stack(list(features.values()), axis=1)
+
+    labels = tf.dtypes.cast(labels, tf.float32) # reshape Labels
+    labels = tf.reshape(labels, [32, 1])
+    labels = tf.transpose(labels)
+
     return features, labels
+
+def input_to_dataset(dataframe):
+    response = dataframe.pop('y')
+    dataset = tf.data.Dataset.from_tensor_slices((dict(dataframe), response.values))
+
+    def input_pack_features_vector(features, labels):
+        features = tf.stack(list(features.values()), axis=1)
+    dataset = dataset.map(input_pack_features_vector)
+
+    return dataset
 
 class LogisticLayer(keras.layers.Layer):
     def __init__(self, input_dim=146, num_outputs=1):
@@ -29,7 +45,6 @@ class LogisticLayer(keras.layers.Layer):
         self.w = tf.Variable(
             initial_value=w_init(shape=(input_dim, num_outputs), dtype='float32'),
             trainable=True)
-        #print(self.w)
 
     def call(self, features):
         return tf.matmul(features, self.w)
@@ -43,17 +58,21 @@ class LogisticModel(tf.keras.Model):
         x = self.logistic_layer(features)
         return x
 
+
+optimizer = tf.optimizers.Adam(learning_rate=0.01)
+
 def hypothesis(logit):
     return tf.divide(1.0, 1.0 + tf.exp(-1.0*logit))
 
 def cost(logits, labels):
     hypo = hypothesis(logits)
-    #return -tf.reduce_mean(labels*tf.math.log(hypo) + (1.0-labels)*tf.math.log(1.0-hypo))
     return -tf.reduce_mean(tf.math.log(hypo)*labels + tf.math.log(1.0-hypo)*(1.0-labels))
 
-def grad(logits, labels):
+#def grad(logits, labels):
+def grad(model, logits, labels):
     with tf.GradientTape() as tape:
-        cost_value = cost(model(features), labels)
+        #cost_value = cost(model(features), labels)
+        cost_value = cost(logits, labels)
     return cost_value, tape.gradient(cost_value, model.trainable_variables)
 
 def train(model, features, labels):
@@ -78,78 +97,72 @@ def batch_accuracy(hypos, labels, batch_size=32):
         #print(hypo, " => ", prediction, " : ", label)
     return accuracy/batch_size
 
+
+def KFoldValidation(df):
+    batch_size = 32
+
+    train_len, test_size = divmod(len(df), batch_size)
+    if test_size == 0:
+        train_len -= 1
+        test_size = batch_size
+
+    train_df = df.loc[:train_len*batch_size-1]
+    test_df = df.loc[train_len*batch_size-1:]
+
+    models = list()
+    train_cost = list()
+    val_accuracy = list()
+
+    kf = KFold(n_splits=train_len)
+    for train_index, val_index in kf.split(train_df):
+        model = LogisticModel()
+
+        train_ds = df_to_dataset(train_df.iloc[train_index])
+        train_dataset = train_ds.map(pack_features_vector)
+        for features, labels in train_dataset:
+            current_cost = train(model, features, labels)
+            train_cost.append(current_cost)
+
+        val_ds = df_to_dataset(train_df.iloc[val_index])
+        val_dataset = val_ds.map(pack_features_vector)
+        for features, labels in val_dataset:
+            logits = model(features)
+            hypos = hypothesis(logits)
+            
+            accuracy = batch_accuracy(tf.reshape(hypos, [1, 32]).numpy()[0], labels.numpy()[0])
+            val_accuracy.append(accuracy)
+        
+        models.append(model)
+    
+    print("Train Cost", train_cost)
+    print("Val Accuracy", val_accuracy)
+
 if __name__ == "__main__":
     df = pd.read_csv("./dataset/dataset_hide-on-bush_enemy.csv")
     df = df.astype(float)
     df = df.sample(frac=1).reset_index(drop=True)   # shuffle
-    
-    train_len, test_len = divmod(len(df), 32)
-    if test_len == 0:
-        train_len -= 32
-        test_len = 32
-    
-    
-    
 
-'''
-if __name__ == "__main__":
-    df = pd.read_csv("./dataset/dataset_hide-on-bush_enemy.csv")
-    df = df.astype(float)
+    #KFoldValidation(df)
 
-    train_df, test_df = train_test_split(df, test_size=0.1)
-    train_df, val_df = train_test_split(train_df, test_size=0.1)
+    batch_size = 32
 
+    train_len, test_size = divmod(len(df), batch_size)
+    if test_size == 0:
+        train_len -= 1
+        test_size = batch_size
 
-    train_ds = df_to_dataset(train_df, shuffle=True)
-    val_ds = df_to_dataset(val_df, shuffle=False)
-    test_ds = df_to_dataset(test_df, shuffle=False)
+    train_df = df.loc[:train_len*batch_size-1]
+    test_df = df.loc[train_len*batch_size-1:]
 
-
+    train_ds = df_to_dataset(train_df)
     train_dataset = train_ds.map(pack_features_vector)
-    #for features, labels in train_dataset.take(1):
-    #    print(features)
-
-    ex_ds = df_to_dataset(df, shuffle=True)
-    ex_dataset = ex_ds.map(pack_features_vector)
-
 
     model = LogisticModel()
-    optimizer = tf.optimizers.Adam(learning_rate=0.01)
-
-    f = open('./model_results/version_1/cost.csv', 'w', newline='')
-    w = open('./model_results/version_1/weights.csv', 'w', newline='')
-    accu = open('./model_results/version_1/accuracy.txt', 'w')
-    cost_record = open('./model_results/version_1/cost.txt', 'w')
-
-    Ws = list()
-    train_cost_results = list()
-    train_accuracy_results = list()
-
-    epochs = range(25)
-    for epoch in epochs:
-        print('epoch : ', epoch)
-        temp_accuracy = list()
-        temp_cost = list()
-        for features, labels in ex_dataset:
-            labels = tf.dtypes.cast(labels, tf.float32) # reshape Labels
-            labels = tf.reshape(labels, [32, 1])
-            labels = tf.transpose(labels)
-
-            current_cost = train(model, features, labels)
-
-            logits = model(features)
-            hypos = hypothesis(logits)
-
-            accuracy = batch_accuracy(tf.reshape(hypos, [1, 32]).numpy()[0], labels.numpy()[0])
-            
-            accu.write(str(accuracy))
-            accu.write("\n")
-            cost_record.write(str(current_cost.numpy()))
-            cost_record.write("\n")
-
-            temp_accuracy.append(accuracy)
-            temp_cost.append(current_cost.numpy())
-        
-        train_accuracy_results.extend(temp_accuracy)
-        train_cost_results.extend(temp_cost)
-'''
+    
+    
+    costs = list()
+    for features, labels in train_dataset:
+        current_cost = train(model, features, labels)
+        costs.append(current_cost.numpy())
+    
+    
